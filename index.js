9fed95b74c04961dddee33e4f928ea08bf203db3
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-08-05 17:17:42 -0400

    I filled in the index.js file. the GET's are working properly. I am getting an 'SQLITE_MISUSE' error in postman when I check the PUT, so I am currently googling that error.

--- index.js
+++ index.js
@@ -1,1 +1,75 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.unsubscribe(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Node.js and Express');
+});
+
+server.get('/users', async (req, res) => {
+    try {
+        const users = await db.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+server.get('/users/:id', async (req, res) => {
+    try {
+        const users = await db.findById(req.params.id);
+        if (users) {
+            res.status(200).json(users);
+        } else {
+            res.status(404).json({ message: 'Could not find this user' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+server.put('/users/:id', async (req, res) => {
+    try {
+        const users = await db.update(req.params.id, req.body);
+        if (users) {
+           res.status(200).json(users);
+        } else {
+            res.status(404).json({ message: 'User could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+server.post('/users', async (req, res) => {
+    try {
+        const users = await db.insert(req.body);
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+server.delete('/user/:id', async (req, res) => {
+    try {
+        const count = await db.remove(req.params.id);
+        if (users) {
+            res.status(200).json({ message: 'User was successfully deleted' });
+        } else {
+            res.status(404).json({ message: 'This user could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error deleting this user' });
+    }
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n*** api running on port ${port} ***\n`));
